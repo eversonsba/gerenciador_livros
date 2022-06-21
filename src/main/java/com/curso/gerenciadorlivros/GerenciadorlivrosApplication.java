8e0fb745eee4a2e74d655738543c136dda960928
Everson Paulino <everson.paulino@gmail.com>
2022-06-20 22:29:23 -0300

    First Commit: criaçao da estrutura do projeto

---
+++ src/main/java/com/curso/gerenciadorlivros/GerenciadorlivrosApplication.java
@@ -0,0 +1,13 @@
+package com.curso.gerenciadorlivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorlivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorlivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/gerenciadorlivros/GerenciadorlivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.gerenciadorlivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorlivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
